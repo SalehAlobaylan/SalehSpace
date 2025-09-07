e8c30ee5c52d42da6f35fcf8583595954bff2d70
Saleh Alobaylan <salehalobaylan@Salehs-MacBook.local>
2025-09-07 15:30:04 +0300

    add main text and socials component

---
+++ src/components/mainText.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function MainText() {
+    return (
+        <div className="text-center">
+            <h1>I started this project in 2017 as a first project for me to learn coding
+                but i dropped it because i don't know what to put in it :)
+                <br />
+                now after more than 8 yaers i decided to rebuild the project but still i don't know what to put in it :
+                <br />
+                but at the end, i just want a space for me in this internet :)
+            </h1>
+        </div>
+    )
+}
