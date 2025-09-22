f13f7ccfca370f7bab6164935be3d1c8e3d09e6e
Salehalobilan <salehwleed1@gmail.com>
2025-09-22 14:33:43 +0300

    matching background colors with main branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
 
       {/* footer */}
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <p>Copyright © 2025 SalehSpace</p>
+        {/* <p>Copyright © 2025 SalehSpace</p> */}
       </footer>
     </div>
   );
